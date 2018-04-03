ee15108d69243a4b39bf4663c32d35fb11b35e2a
FloreaHorea <37344652+FloreaHorea@users.noreply.github.com>
2018-04-04 01:22:16 +0300

    change

---
+++ change.js
@@ -0,0 +1,13 @@
+console.log('hello there')
+console.log('hello there', 'gordon')
+var todos = ['item 1', 'item 2', 'item 3']
+console.log(todos)
+console.log('My todos',todos)
+todos.push('item 4')
+todos.push('item 5')
+todos[0]
+todos[1]
+todos[2]
+todos[3]
+todos[4]
+todos[0] = 'item 1 updated'
